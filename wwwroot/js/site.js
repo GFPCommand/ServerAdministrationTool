02f12c2544ccf3f330c3e6f2a696c43cac18cbab
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2023-08-03 23:36:48 +0300

    Added animations; Modified info;

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,8 +4,14 @@ var appsList = document.getElementById('apps');
 var usersElem = document.getElementById('users-list');
 var usersList = document.getElementById('users');
 
+var openImgApps = appsElem.firstElementChild;
+var openImgUsers = usersElem.firstElementChild;
+
 var isAppsListOpen  = false;
 var isUsersListOpen = false;
+
+appsElem.style.userSelect = 'none';
+usersElem.style.userSelect = 'none';
 
 appsElem.onclick = function(event) {
     event.preventDefault();
@@ -13,13 +19,17 @@ appsElem.onclick = function(event) {
     if (isAppsListOpen) {
         appsList.style.maxHeight = '0';
         appsList.style.transition = '0.5s ease-out';
+
+        openImgApps.style.transform = 'rotate(45deg)';
+
         isAppsListOpen = false;
-        appsElem.textContent = 'Показать';
     } else {
         appsList.style.maxHeight = '100vh';
         appsList.style.transition = '0.5s ease-in';
+
+        openImgApps.style.transform = 'rotate(0deg)';
+
         isAppsListOpen = true;
-        appsElem.textContent = 'Скрыть'
     }
 }
 
@@ -29,13 +39,16 @@ usersElem.onclick = function(event) {
     if (isUsersListOpen) {
         usersList.style.maxHeight = '0';
         usersList.style.transition = '0.5s ease-out';
-        isUsersListOpen = false;
-        usersElem.textContent = 'Показать';
 
+        openImgUsers.style.transform = 'rotate(45deg)';
+
+        isUsersListOpen = false;
     } else {
         usersList.style.maxHeight = '100vh';
         usersList.style.transition = '0.5s ease-in';
+
+        openImgUsers.style.transform = 'rotate(0deg)';
+
         isUsersListOpen = true;
-        usersElem.textContent = 'Скрыть';
     }
 }
