8bd2babbfe3985db2eed1180011299b1335f4dc9
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2023-08-16 16:36:53 +0300

    Add files via upload

--- Helpers/Applications.cs
+++ Helpers/Applications.cs
@@ -21,20 +21,46 @@ namespace Helpers {
         public static HtmlString GetApplicationsList() {
             StringBuilder sb = new StringBuilder();
 
-            for (int i = 1; i <= 10; i++){
+            int[] arr = new int[11];
+
+            Random r = new Random();
+
+            for (int i = 1; i <= arr.Length-1; i++){
+                arr[i] = r.Next(3);
+
+                string state = string.Empty;
+                string color = string.Empty;
+
+                switch (arr[i])
+                {
+                    case 0:
+                        state = "OK";
+                        color = "#BDECB6"; // светло-зеленый
+                        break;
+                    case 1:
+                        state = "Failed";
+                        color = "#FA8072"; // salmon
+                        break;
+                    case 2:
+                        state = "Stopped";
+                        color = "#D3D3D3"; // светло-серый
+                        break;
+                }
+
                 sb.Append($@"
-                    <div class='app-item'>
+                    <div class='app-item' style='background-color: {color}'>
 		                <img src='/src/hamburger-menu.png' alt='image for application' style='width: 5vw;'>
 		                <div class='app-info'>
 			                <span>Приложение {i}</span>
 			                <br>
-			                <span>Состояние: ОК</span>
+			                <span>Состояние: {state}</span>
 		                </div>
 		                <div class='app-control'>
 			                <a class='control'>Запуск</a>
 			                <a class='control'>Перезапуск</a>
 			                <a class='control'>Остановка</a>
                             <a class='control'>Обновление</a>
+                            <a class='control'>Информация</a>
 		                </div>
 	                </div>
                 ");
