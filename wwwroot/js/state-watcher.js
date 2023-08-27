5ab72fb9df8f144f613a76035242d302eae26c47
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2023-08-27 23:04:29 +0300

    ajax state updater prototype

---
+++ wwwroot/js/state-watcher.js
@@ -0,0 +1,14 @@
+﻿function update() {
+	$.ajax({
+		type: "GET",
+		url: "/StateWatcher/CheckState",
+		success: function (data) {
+			$("h2").text(data);
+		},
+		error: function (data) {
+			console.error(data);
+		}
+	});
+}
+
+let func = setInterval(update, 500);
