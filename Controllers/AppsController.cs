2b0552f1b1cdd2df227b0d2761ce7b3a7b10f1b3
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2023-09-05 08:00:11 +0300

    Add files via upload

--- Controllers/AppsController.cs
+++ Controllers/AppsController.cs
@@ -1,25 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Server_Administration_Tool.Controllers
 {
     public class AppsController : Controller
     {
+        private const string START_APP = "start";
+        private const string STOP_APP = "stop";
+        private const string RESTART_APP = "restart";
+        private const string RELOAD_APP = "reload";
+
         public void RestartApplication(string app)
         {
             //TODO:
             //restart app: if Linux OS -> systemctl restart {app}
+
+            ServerAppAction(app, RESTART_APP);
         }
 
         public void ReloadApplication(string app)
         {
             //TODO:
             //reload configs and restart app: if Linux OS -> systemctl reload {app}
+
+            ServerAppAction(app, RELOAD_APP);
         }
 
         public string StartApplication(string app)
         {
             //TODO:
             //start app: if Linux OS -> systemctl start {app}
+
+            ServerAppAction(app, START_APP);
+
             return app;
         }
 
@@ -27,6 +41,32 @@ namespace Server_Administration_Tool.Controllers
         {
             //TODO:
             //stop app: if Linux OS -> systemctl stop {app}
+            
+            ServerAppAction(app, STOP_APP);
+        }
+
+        private void ServerAppAction(string appName, string action)
+        {
+            if (!OperatingSystem.IsLinux()) return;
+
+            try
+            {
+                ProcessStartInfo info = new();
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+                info.FileName = "/bin/bash";
+                info.UseShellExecute = false;
+                info.Arguments = $"/c systemctl {action} {appName}";
+
+                using Process process = new() {StartInfo = info};
+
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch
+            {
+                return;
+            }            
         }
     }
 }
